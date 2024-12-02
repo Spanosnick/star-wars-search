d2de90a474e7b3860180a22b4379207d91cff6fb
Spanos <nickspanosf1@gmail.com>
2024-12-02 23:12:18 +0200

    separate Film Card and css

--- src/components/FilmCard.js
+++ src/components/FilmCard.js
@@ -1,14 +1,20 @@
 import React from "react";
+import './FilmCard.css';
 
 export function FilmCard(props) {
     const {title,director,producer,release_date,url} = props;
+    function showDetails() {
+        console.log(url);
+
+    }
+    
     return (
         <div className='generalCard'>
-            <h2>Title:{title}</h2>
-            <p>Director{director}</p>
-            <p>Producer:{producer}</p>
-            <p>Release Date:{release_date}</p>
-            <button>Details{url}</button>
+            <h2 className='filmTitle'> {title}</h2>
+            <p className='filmDirector'>Director: <span>{director}</span> </p>
+            <p className='filmProducer'>Producer: <span>{producer}</span></p>
+            <p className='filmReleaseDate'>Release Date: <span>{release_date}</span></p>
+            <button onClick={(url)=> showDetails(url)}> Details </button>
         </div>
     );
 }
